d1138e9555979d688ff0b4d0ae22a32ac2f3f14f
simonlearnscoding <simon.muscas@gmail.com>
2023-05-27 14:00:46 +0200

    second

--- practicing_observer_pattern.py
+++ practicing_observer_pattern.py
@@ -1,4 +1,3 @@
-Eventmanager
 
 class WeatherStation:
   __init__():
@@ -9,19 +8,32 @@ class WeatherStation:
   def update_weather(self, weather_type, degrees):
     for agent in self.subscribers[topic]:
       agent.notify(weather_type, degrees):
-  
-  def notify(self, weather_type, degrees):
+
+ def subscribe(self, weather_type, agent):
+    self.subscribers[weather_type].append(agent)
+
      
 Weather_Station_Houston = WeatherStation()    
 
+
+class parentClass(Weather_Station_Houston):
+   __init__(interests):
+    for interest in interests:
+       Weather_Station_Houston.subscribe(interest, self)
+        
+    def notify(self, weather_type, degrees):
+        print("Weather App: It is {} degrees and {} in Houston".format(degrees, weather_type))
+
+        
 class WeatherApp(Weather_Station_Houston):
    __init__(interests):
+        super().__init__(interests) 
 
-    for interest in interests:
-       Weather_Station_Houston.(interest, self)
-     
 class NewsChannel:
+   __init__(interests):
+        super().__init__(interests) 
+    
 class Farmer:
-
-
-
+   __init__(interests):
+        super().__init__(interests) 
+     
